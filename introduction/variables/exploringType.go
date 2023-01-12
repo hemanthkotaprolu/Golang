748d78f219fa86857ba3f24f31bbd6acbb06cea2
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-12 20:35:24 +0530

    complete zero-type video

---
+++ introduction/variables/exploringType.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var y = 42
+var z string = "hemanth"
+
+// DECLARED VARIABLE can't be changed. 
+// GO is a STATICALLY TYPED language
+// variable is declared only to store a particular type!!
+
+
+func main() {
+    fmt.Println(y)
+    fmt.Printf("%T\n",y)
+
+    fmt.Printf("%T\n", z)
+}
---
+++ introduction/variables/zeroType.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var y2 string
+
+func main() {
+    fmt.Println("printing the value of y:", y, "ending")
+    fmt.Printf("%T\n", y)
+    
+
+    y2 = "sam"
+}
