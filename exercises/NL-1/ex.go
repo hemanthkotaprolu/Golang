77ac6a70fbd6662530359b1995316c6db6582544
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-29 12:56:18 +0530

    add NL-1

---
+++ exercises/NL-1/ex.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    /********************************************/
+    x1 := 42
+    y1 := "James Bond"
+    z1 := true
+
+    fmt.Println(x1,y1,z1)
+    /********************************************/
+    
+    var x2 int
+    var y2 string
+    var z2 bool
+
+    
+}
