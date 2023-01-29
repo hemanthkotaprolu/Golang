6e919d6ffe185e6892dcc85e165831a7413a1ea8
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-29 11:32:35 +0530

    add ownType.go

---
+++ introduction/variables/ownType.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// we can use "type" keyword to create out own type
+// type <name> <primitive-type>
+type hotdog int
+
+
+var b hotdog
+
+func main() {
+    fmt.Println("Hello!!")
+}
