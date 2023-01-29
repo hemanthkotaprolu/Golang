f1c4b6e01a01bf0c731ad5406ce67195d6d06514
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-29 21:13:28 +0530

    add constantType.go

---
+++ programmingFunda/stringType.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    s := "Hello, world!!"
+
+    fmt.Println(s)
+    fmt.Printf("%T\n", s)
+
+    bs := []byte(s)
+    fmt.Println(bs)
+
+    fmt.Printf("%T\n", bs)
+    fmt.Printf("%#U\n", s[0])
+}
