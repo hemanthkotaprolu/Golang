59c9a73201c83f42dd96c914a5b5249c37c31413
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-30 20:59:01 +0530

    add bitShifting.go

---
+++ programmingFunda/bitShifting.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    x := 2
+
+    fmt.Printf("%d\t\t%b", x, x)
+    fmt.Println()
+
+    
+    y := x << 1
+    fmt.Printf("%d\t\t%b", y, y)
+    fmt.Println()
+    
+}
