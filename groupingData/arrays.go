b2b10ad8af6dc0dcffda0e9a7c1d301acd1ea473
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-01-31 21:52:13 +0530

    format after slice

--- groupingData/arrays.go
+++ groupingData/arrays.go
@@ -2,9 +2,8 @@ package main
 
 import "fmt"
 
-
 func main() {
-    var x [10] string 
-    fmt.Println(x)
-    fmt.Println(len(x))
+	var x [10]string
+	fmt.Println(x)
+	fmt.Println(len(x))
 }
--- groupingData/slices.go
+++ groupingData/slices.go
@@ -4,27 +4,28 @@ import "fmt"
 
 func main() {
 
-    // x := type{values}
-    
-    x := []int{1,2,3,4,5,6}
+	// x := type{values}
 
-    fmt.Println(len(x))
+	x := []int{1, 2, 3, 4, 5, 6}
 
+	fmt.Println(len(x))
 
-    // for range
-    for i, v := range x {
-        fmt.Printf("%v - %v\n", i, v)
-    }
+	// for range
+	for i, v := range x {
+		fmt.Printf("%v - %v\n", i, v)
+	}
 
-    // slicing a slice
-    y := x[1:4]
-    fmt.Println(y)
+	// slicing a slice
+	y := x[1:4]
+	fmt.Println(y)
 
-    // append a slice
-    z := append(x, x...)
+	// append a slice
+	z := append(x, x...)
 
-    fmt.Println(len(z))
-    a := make([]int, 5, 10)
-    fmt.Println(a)
-    fmt.Println(cap(a))
+	fmt.Println(len(z))
+
+	// if the size of the slice exceed then the length doubled
+	a := make([]int, 5, 10)
+	fmt.Println(a)
+	fmt.Println(cap(a))
 }
