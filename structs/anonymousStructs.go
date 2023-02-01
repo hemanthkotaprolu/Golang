244a575da5e10cce7cf457c8dc0e62fff4e6d4cb
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-01 23:18:55 +0530

    add anoymousStructs.go

---
+++ structs/anonymousStructs.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+type person struct {
+    first string
+    last string
+    age int
+}
+*/
+
+func main() {
+    /*
+    p1 := person {
+        first: "James",
+        last: "Bond",
+        age: 32,
+    }
+    */
+
+    p1 := struct {
+        first string
+        last string
+        age int
+    }{
+        first: "James",
+        last: "Bond",
+        age: 32,
+    }
+
+    fmt.Println(p1)
+}
