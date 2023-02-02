7b3f825dff31753c105699eb013203b5db2f03c1
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-02 20:37:11 +0530

    modified

--- functions/passingSliceArgument.go
+++ functions/passingSliceArgument.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
     x1 := []int{1,2,3,4,5,6,7,8,9}
 
-    fmt.Println(getSum(x1))
+    fmt.Println(getSum(x1)
 }
 
-func getSum(x []int ) int {
+func getSum(x []int) int {
     sum := 0
 
     for _, v := range x {
