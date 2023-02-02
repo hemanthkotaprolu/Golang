e6106ffc9fbafeb15e9581e789f5e01751ee9218
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-02 21:55:32 +0530

    add deferKeyword.go

--- functions/passingSliceArgument.go
+++ functions/passingSliceArgument.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
     x1 := []int{1,2,3,4,5,6,7,8,9}
 
-    fmt.Println(getSum(x1)
+    fmt.Println(getSum(x1))
+
+    sample("James", 190, 1,2,3,4)
 }
 
 func getSum(x []int) int {
@@ -15,4 +17,10 @@ func getSum(x []int) int {
         sum += v
     }
     return sum
+}
+
+
+func sample(s string, x1 int, x2 ...int) {
+    fmt.Println(x1)
+    fmt.Println(x2)
 }
