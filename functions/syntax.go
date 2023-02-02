ec41fb75cf29a3ab282856855db22054bbd4ea86
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-02 20:02:25 +0530

    start functions

---
+++ functions/syntax.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+    sayHello()
+
+    sayHelloName("hemanth")
+
+    s1 := sayHii("Hemanth")
+    fmt.Println(s1)
+
+    x, y := mouse("Hemanth", "Kotaprolu")
+    fmt.Println(x)
+    fmt.Println(y)
+    // fmt.Println("Hello")
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { ... }
+
+func sayHello() {
+    fmt.Println("Hello")
+}
+
+// everything in GO is "PASS BY VALUE"
+func sayHelloName(s string) {
+    fmt.Println("Hello", s)
+}
+
+func sayHii(str string) string {
+    return fmt.Sprint("Hello from woo, ", str)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+    a := fmt.Sprint(fn, ln, `, says Hello`)
+    b := false
+
+    return a, b
+}
---
+++ functions/variadicParameters.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(foo(1,2,3,4,5,6,7,8,9))
+}
+
+func foo(x ...int) int {
+    fmt.Println(x)
+    fmt.Printf("%T\n", x)
+
+    sum := 0
+
+    for _, v := range x {
+        sum += v
+    }
+
+    return sum
+}
