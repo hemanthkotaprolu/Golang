1882b4acde2c5ef75a0e2e7f42ede84d389628ce
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2023-02-02 20:33:40 +0530

    add passingSliceArguement.go

---
+++ functions/passingSliceArgument.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    x1 := []int{1,2,3,4,5,6,7,8,9}
+
+    fmt.Println(getSum(x1))
+}
+
+func getSum(x []int ) int {
+    sum := 0
+
+    for _, v := range x {
+        sum += v
+    }
+    return sum
+}
